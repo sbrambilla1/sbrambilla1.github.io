d8246bf63ee8f0fe0855ea20f330cfef2477b7e5
sbrambilla1 <stefano.brambilla43@gmail.com>
2026-05-18 12:49:48 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  // Avvia il service worker
+});
+
+self.addEventListener('fetch', (e) => {
+  // Gestisce le richieste web (lasciato vuoto per caricamento live standard)
+});
